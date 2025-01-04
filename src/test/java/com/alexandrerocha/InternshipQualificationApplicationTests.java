bc0ada57fa0b1c931500c249f27476540cf5c0b1
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2025-01-04 00:56:41 +0000

    Initial commit

---
+++ src/test/java/com/alexandrerocha/InternshipQualificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrerocha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternshipQualificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
