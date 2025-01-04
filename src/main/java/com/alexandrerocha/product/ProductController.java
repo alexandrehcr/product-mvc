b9728a4f0ad3f6d39a5ee6d9dda67e8dea765c2c
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2025-01-04 15:17:53 +0000

    Add product registration validation and HTTP Status Code

--- src/main/java/com/alexandrerocha/product/ProductController.java
+++ src/main/java/com/alexandrerocha/product/ProductController.java
@@ -2,11 +2,16 @@ package com.alexandrerocha.product;
 
 import com.alexandrerocha.product.dto.ProductRegistrationDto;
 import com.alexandrerocha.product.repository.ProductRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/products")
 public class ProductController {
 
+    private static final Logger log = LoggerFactory.getLogger(ProductController.class);
+    
     @Autowired
     ProductRepository repository;
     
@@ -30,16 +37,28 @@ public class ProductController {
     }
     
     @PostMapping("/register")
-    public String registerProduct(@Valid ProductRegistrationDto productRegistrationDto, Model model) {
-        var product = ProductMapping.mapToEntity(productRegistrationDto);
+    public String registerProduct(@Valid @ModelAttribute("productDto") 
+                                      ProductRegistrationDto registrationDto,
+                                  BindingResult bindingResult, Model model,
+                                  HttpServletResponse response) {
+
+        if (bindingResult.hasErrors()) {
+            log.info(bindingResult.getAllErrors().toString());
+            response.setStatus(400);
+            return "productRegistration";
+        }
+        
+        var product = ProductMapping.mapToEntity(registrationDto);
         repository.save(product);
         
         var products = repository.findByOrderByPriceAsc()
                 .stream()
                 .map(ProductMapping::mapToDto)
                 .toList();
+        
         model.addAttribute("products", products);
         
+        response.setStatus(201);
         return "productCatalogue";
     }
 }
--- src/main/java/com/alexandrerocha/product/dto/ProductRegistrationDto.java
+++ src/main/java/com/alexandrerocha/product/dto/ProductRegistrationDto.java
@@ -2,18 +2,22 @@ package com.alexandrerocha.product.dto;
 
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
 import java.math.BigDecimal;
 
 public class ProductRegistrationDto {
     @NotBlank(message = "O nome do produto não pode ser vazio.")
-    @Length(min = 2, max = 100)
+    @Length(min = 2, max = 100, message = "O nome deve conter de 2 a 100 caracteres.")
     String name;
 
+    @NotBlank(message = "A descrição do produto não pode ser vazia.")
+    @Length(max = 1000, message = "A descrição deve conter até 1000 caracteres.")
     String description;
 
-    @Min(0)
+    @Min(value = 0, message = "O preço do produto não pode ser negativo.")
+    @NotNull(message = "Defina um preço para o produto.")
     BigDecimal price;
 
     boolean available;
