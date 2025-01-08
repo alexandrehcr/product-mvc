9196d89b0524c25015cb41c5beb3a137f9b30e19
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2025-01-08 18:43:19 +0000

    Add tests for product update and deletion

--- src/test/java/com/alexandrerocha/product/ProductControllerIntegrationTest.java
+++ src/test/java/com/alexandrerocha/product/ProductControllerIntegrationTest.java
@@ -2,7 +2,7 @@ package com.alexandrerocha.product;
 
 import com.alexandrerocha.product.dto.ProductSubmissionDto;
 import com.alexandrerocha.product.repository.ProductRepository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,10 +11,10 @@ import org.springframework.test.web.servlet.MockMvc;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.http.MediaType.APPLICATION_FORM_URLENCODED;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -26,56 +26,58 @@ class ProductControllerIntegrationTest {
 
     @Autowired
     ProductRepository repository;
-
+    
+    final String INVALID_HUGE_NAME = "Huge name".repeat(12); // length = 108
+    final String INVALID_HUGE_DESCRIPTION = "Huge Description".repeat(63); // length = 1008
+    
 
     @Test
-    void listProducts() throws Exception {
+    void showProductsListing() throws Exception {
         mockMvc.perform(get("/products"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("productListing"))
-                .andExpect(model().attributeExists("products"));
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("productListing"),
+                        model().attributeExists("products"));
     }
-
+    
     @Test
     void showRegisterProductForm() throws Exception {
         mockMvc.perform(get("/products/register"))
-                .andExpect(status().isOk())
-                .andExpect(view().name("productRegistration"));
+                .andExpectAll(
+                        status().isOk(),
+                        view().name("productRegistration"));
     }
 
     @Test
-    void registerProductBadRequestWithEmptyData() throws Exception {
+    void registerProduct_EmptyData_BadRequest() throws Exception {
         mockMvc.perform(
                         post("/products/register")
                                 .contentType(APPLICATION_FORM_URLENCODED)
                                 .param("name", "")
                                 .param("description", "")
                                 .param("price", ""))
-                .andExpect(status().isBadRequest())
-                .andExpect(model().hasErrors());
+                .andExpectAll(
+                        status().isBadRequest(),
+                        model().hasErrors());
     }
 
 
     @Test
-    void registerProductBadRequestWithDataBeyondLimit() throws Exception {
-        String hugeName = "ABCDE".repeat(21); // length = 105
-        String hugeDescription = "HugeDescription".repeat(67); // length = 1005
-        
+    void registerProduct_InputTooLarge_BadRequest() throws Exception {
         mockMvc.perform(
                         post("/products/register")
                                 .contentType(APPLICATION_FORM_URLENCODED)
-                                .param("name", hugeName)
-                                .param("description", hugeDescription)
+                                .param("name", INVALID_HUGE_NAME)
+                                .param("description", INVALID_HUGE_DESCRIPTION)
                                 .param("price", "1"))
-                .andExpect(status().isBadRequest())
-                .andExpect(model().hasErrors())
-                .andExpect(model().errorCount(2));
+                .andExpectAll(
+                        status().isBadRequest(),
+                        model().hasErrors(),
+                        model().errorCount(2));
     }
-    
-    
 
     @Test
-    void registerProductCreated() throws Exception {
+    void registerProduct_ValidInputs() throws Exception {
         var validDto = new ProductSubmissionDto();
         validDto.setName("Test Name");
         validDto.setDescription("Test Description");
@@ -89,15 +91,125 @@ class ProductControllerIntegrationTest {
                                 .param("description", validDto.getDescription())
                                 .param("price", String.valueOf(validDto.getPrice()))
                                 .param("isAvailable", String.valueOf(validDto.getIsAvailable())))
-                .andExpect(status().isCreated())
-                .andExpect(model().hasNoErrors())
-                .andDo(print());
-
+                .andExpectAll(
+                        status().isCreated(),
+                        model().hasNoErrors());
+        
         var savedProduct = repository.findByName(validDto.getName())
                 .orElseThrow(() -> new AssertionError("Product not found."));
         assertEquals(validDto.getName(), savedProduct.getName());
         assertEquals(validDto.getDescription(), savedProduct.getDescription());
         assertEquals(validDto.getPrice(), savedProduct.getPrice());
         assertEquals(validDto.getIsAvailable(), savedProduct.getIsAvailable());
+    }
+    
+    @Nested
+    class UpdateProductTests {
+        
+        Product saved;
+        Long id;
+        
+        @BeforeEach
+        void setup() {
+            saved = repository.save(new Product(null, "Test Name", "Test description", new BigDecimal("123.00"), true));
+            id = saved.getId();
+        }
+        
+        @AfterEach()
+        void cleanup() {
+            repository.deleteById(id);
+        }
+        
+        @Test
+        void showProductUpdatePage() throws Exception {
+            mockMvc.perform(get("/products/edit/{id}", id))
+                    .andExpectAll(
+                            status().isOk(),
+                            view().name("productEditing"));
+        }
+        
+        @Test
+        void updateProduct_validInputs() throws Exception {
+            var updatedProductDto = ProductMapping.mapToProductSubmissionDto(saved);
+            updatedProductDto.setName("Updated Test Name");
+            updatedProductDto.setDescription("Updated Description");
+            updatedProductDto.setPrice(new BigDecimal("321.00"));
+            updatedProductDto.setIsAvailable(false);
+
+            mockMvc.perform(post("/products/edit/{id}", id)
+                            .param("name", updatedProductDto.getName())
+                            .param("description", updatedProductDto.getDescription())
+                            .param("price", String.valueOf(updatedProductDto.getPrice()))
+                            .param("isAvailable", String.valueOf(updatedProductDto.getIsAvailable())))
+                    .andExpect(status().isOk());
+            
+            saved = repository.findById(id).orElseThrow(() -> new AssertionError("Product not found."));
+            assertEquals(saved.getName(), updatedProductDto.getName());
+            assertEquals(saved.getDescription(), updatedProductDto.getDescription());
+            assertEquals(saved.getPrice(), updatedProductDto.getPrice());
+            assertEquals(saved.getIsAvailable(), updatedProductDto.getIsAvailable());
+        }
+        
+        @Test
+        void updateProduct_InputTooLarge_BadRequest() throws Exception {
+            var invalidProductDto = new ProductSubmissionDto();
+            invalidProductDto.setName(INVALID_HUGE_NAME);
+            invalidProductDto.setDescription(INVALID_HUGE_DESCRIPTION);
+
+            mockMvc.perform(post("/products/edit/{id}", id)
+                            .param("name", invalidProductDto.getName())
+                            .param("description", invalidProductDto.getDescription()))
+                    .andExpectAll(
+                            status().isBadRequest(),
+                            model().hasErrors());
+        }
+
+        @Test
+        void updateProduct_EmptyData_BadRequest() throws Exception {
+            mockMvc.perform(post("/products/edit/{id}", id)
+                            .param("name", "")
+                            .param("description", ""))
+                    .andExpect(status().isBadRequest())
+                    .andExpect(model().hasErrors());
+        }
+    }
+    
+    @Nested
+    class DeleteProductTests {
+        Product saved;
+        Long id;
+
+        @BeforeEach
+        void setup() {
+            saved = repository.save(new Product(null, "Test Name", "Test description", new BigDecimal("123.00"), true));
+            id = saved.getId();
+        }
+        
+        @Test
+        void deleteProduct_ProductExists_RedirectAndNotify() throws Exception {
+            mockMvc.perform(get("/products/delete/{id}", id))
+                    .andExpectAll(
+                            status().isFound(),
+                            flash().attributeExists("notification"),
+                            redirectedUrl("/products")
+                    );
+            
+            assertTrue(repository.findById(id).isEmpty());
+        }
+        
+        @Test
+        void deleteProduct_ProductDoesntExist_NotFound() throws Exception {
+            mockMvc.perform(get("/products/delete/{id}", Long.MAX_VALUE))
+                    .andExpect(status().isNotFound());
+        }
+    }
+        
+    // When the program is running, `GET /non-defined-url` returns the 404.html view and 404 status.
+    @Test
+    @Disabled("Fails probably due to Spring Boot Test error handler behavior; revisit later.")
+    void show404() throws Exception {
+        mockMvc.perform(get("/non-defined-url"))
+                .andExpect(status().isNotFound())
+                .andExpect(view().name("404"));
     }
 }
