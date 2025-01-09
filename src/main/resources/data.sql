f92001587b8207b3d09cbf1e6d40a4b33eef8c55
Alexandre Rocha <alexandrehc.rocha@gmail.com>
2025-01-09 12:27:56 +0000

    Add mocking product records

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,6 +1,34 @@
-INSERT INTO products (name, description, price, is_available) VALUES ('Smart TV', 'See the world as you never did', 899.00, TRUE);
-INSERT INTO products (name, description, price, is_available) VALUES ('Smartphone', 'The world in your hands', 1999.90, TRUE);
-INSERT INTO products (name, description, price, is_available) VALUES ('Laptop', 'Incredibly fast, small and elegant', 4695.99, FALSE);
-INSERT INTO products (name, description, price, is_available) VALUES ('Freezer', 'I''m freezing 🥶', 749.50, TRUE);
-
 ALTER TABLE products ALTER COLUMN description VARCHAR(1000);
+
+INSERT INTO products (name, description, price, is_available) VALUES ('Smart TV', 'Veja o mundo como nunca viu', 1199.99, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Smartphone', 'O mundo em suas mãos', 1999.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Laptop', 'Incrivelmente rápido, pequeno e elegante', 4695.00, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Freezer', 'Estou congelando 🥶', 749.50, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Chuveiro Elétrico', 'Banho quentinho sem levar choque', 149.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Smartwatch', 'Acompanhe sua saúde', 990.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Videogame', 'O melhor desempenho para a sua melhor performance', 2399.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Headset', 'Escute cada detalhe do seu oponente', 499.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Headphone studio', 'Escute suas músicas preferidas como se você estivesse no estúdio de gravação', 799.99, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Earbud bluetooth', 'Menos um fio pra enrolar e os pets destruírem', 390.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Microondas', 'A praticidade concretizada', 399.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Ar-Condicionado', 'Deixe a temperatura do seu jeito', 1189.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Liquidificador', 'Perfeito para sucos e vitaminas', 179.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Air Fryer', 'Frita com ar quente', 390.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Aspirador de Pó', 'Limpeza sem esforço', 239.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Impressora', 'Seus documentos sempre em mãos', 899.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('HD Externo', 'Armazene todos os seus arquivos', 179.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Roteador', 'Internet de alta velocidade', 299.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Cadeira de Escritório', 'Ergonômica e estilosa', 599.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Cadeira Gamer', 'Conforto para maratonas de jogos', 1299.99, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Bicicleta Elétrica', 'Mobilidade urbana ecológica', 2499.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Patinete Elétrico', 'Seu companheiro para trajetos urbanos', 1199.00, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Lavadoura lava e seca', 'Roupas limpas e secas com menos esforço', 1379.99, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('E-Reader', 'Carregue uma biblioteca inteira com você', 639.90, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Câmera de Ação', 'Eternize suas aventuras', 1249.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Cafeteira', 'O café que você quiser em segundos', 229.00, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Impressora 3D', 'Dê vida às suas ideias', 1399.00, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Projetor Portátil', 'O cinema onde você quiser', 149.00, TRUE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Alimentador Automático para Pets', 'Comida fresquinha e água limpinha', 269.90, FALSE);
+INSERT INTO products (name, description, price, is_available) VALUES ('Ventilador de teto', 'Só cai se o teto cair', 259.00, FALSE);
+
+
